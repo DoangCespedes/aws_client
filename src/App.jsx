b239a143465b57506dfec017c0dbfe976d7f92e4
Doang <doangcespedesloreto@gmail.com>
2025-06-04 10:12:27 -0400

    [MOD]textract is included

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
 import { useState } from 'react'
-
-import './App.css'
 import axios from 'axios'
+import './App.css'
 
 function App() {
-
-  const [post, setPost] = useState({
-    title:'',
-    photo:null
-  })
+  const [post, setPost] = useState({ title: '', photo: null });
+  const [filename, setFilename] = useState(null);
+  const [text, setText] = useState(null);
 
 
   const handleSubmit = async(e) => {
     e.preventDefault();
-    const formData = new FormData()
-
-    formData.append('photo', post.photo)
+    const formData = new FormData();
+    formData.append('photo', post.photo);
 
     const response = await axios.post('http://localhost:3000/upload', formData, {
-      headers:{
+      headers: {
         "Content-Type": "multipart/form-data"
       }
-    })
+    });
+    setFilename(response.data.filename);
+    console.log('Archivo subido, filename:', response.data.filename);
+  }
 
-    console.log(response)
+  const handleProcesar = async () => {
+    if (!filename) {
+      alert('Primero sube una imagen');
+      return;
+    }
+    const res = await axios.post('http://localhost:3000/procesar', { filename });
+    setText(res.data.texto)
+    alert('Texto detectado: ' + res.data.texto);
   }
 
   return (
     <>
       <div>
-        <h1>subiendo a S3</h1>
-
+        <h1>Subiendo a S3 y detectando texto</h1>
         <form onSubmit={handleSubmit}>
           <input 
             type='text' 
@@ -42,12 +47,18 @@ function App() {
             name='photo'
             onChange={e => setPost({...post, photo: e.target.files[0]})}
           />
-
-          <button type='submit'> Subir </button>
+          <button type='submit'>Subir</button>
         </form>
+        <button onClick={handleProcesar}>Procesar Imagen</button>
+      </div>
+
+      <div>
+        <p>
+          {text}
+        </p>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
